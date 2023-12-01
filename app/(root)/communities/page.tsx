a1938c77dea9cf787c1b54cefe6c843d108b5cd5
chriskolb00 <cmlkolb@gmail.com>
2023-11-30 23:51:16 -0600

    app commit

---
+++ app/(root)/communities/page.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+const Page = async () => {
+  return (
+    <section>
+        <h1 className="text-text mb-10 text-white">
+            Community
+        </h1>
+    </section>
+  )
+}
+
+export default Page
--- postcss.config.js
+++ postcss.config.js
@@ -3,4 +3,8 @@ module.exports = {
     tailwindcss: {},
     autoprefixer: {},
   },
+  // image:{
+  //   domains: ['utfs.io'],
+  //   format: ['image/avif', 'image/webp']
+  // }
 }
